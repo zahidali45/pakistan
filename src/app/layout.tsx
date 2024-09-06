cdfc65d791025064945037410d1e49fedf7aafe0
tsc <mz260959@gmail.com>
2024-09-06 22:02:44 +0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter} from "next/font/google";
 import "./globals.css";
-import { HeadManagerContext } from "next/dist/shared/lib/head-manager-context.shared-runtime";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <header/>
+        <Header/>
         {children}</body>
     </html>
   );
