8f662fa7cafd741553a59c475669c4481acd6892
tsc <mz260959@gmail.com>
2024-09-06 21:39:22 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
-
+import Image from 'next/image'
 const page = () => {
   return (
     <div><figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-    <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/zahid.jpg" alt="" width="384" height="512"/>
+    <Image className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/zahid.jpg" alt="" width="384" height="512"/>
     <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
       <blockquote>
         <p className="text-lg font-medium">
-          “Tailwind CSS is the only framework that I've seen scale
-          on large teams. It’s easy to customize, adapts to any design,
-          and the build size is tiny.”
+          Tailwind CSS is the only framework that I have seen scale
+          on large teams. It is easy to customize, adapts to any design,
+          and the build size is tiny.
         </p>
       </blockquote>
       <figcaption className="font-medium">
